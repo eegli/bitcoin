f3336af4de072bb836fa122ace734d62716bfd5b
Xiaohw1999 <wuxiaohui990723@gmail.com>
2023-05-30 21:24:31 +0200

    latest

--- explorer/src/pages/AddressTransactions.vue
+++ explorer/src/pages/AddressTransactions.vue
@@ -2,28 +2,27 @@
     <div class="address">
         <h3>Balance: {{ balance }}</h3>
         <h3>Transactions:</h3>
+        <el-row>
+            <el-col :span="6">
+            <el-select v-model="roles" placeholder="Select Role">
+                <el-option label="All" value=""></el-option>
+                <el-option label="Sender" value= sender></el-option>
+                <el-option label="Receiver" value= receiver></el-option>
+            </el-select>
+            </el-col>
+            <!-- <el-col :span="6">
+            <el-select v-model="coinbase" placeholder="Select Coinbase">
+                <el-option label="All" value=""></el-option>
+                <el-option label="Coinbase" value="true"></el-option>
+                <el-option label="No Coinbase" value="false"></el-option>
+            </el-select>
+            </el-col> -->
+            <el-col :span="4">
+                <el-button type="primary" @click="fetchAddress">Search</el-button>
+            </el-col>
+        </el-row>
         <div v-if="transactions.length === 0">No transactions found.</div>
         <div v-else>
-            <el-row>
-                <el-col :span="6">
-                <el-select v-model="roles" placeholder="Select Role">
-                    <el-option label="All" value=""></el-option>
-                    <el-option label="Sender" value= sender></el-option>
-                    <el-option label="Receiver" value= receiver></el-option>
-                </el-select>
-                </el-col>
-                <!-- <el-col :span="6">
-                <el-select v-model="coinbase" placeholder="Select Coinbase">
-                    <el-option label="All" value=""></el-option>
-                    <el-option label="Coinbase" value="true"></el-option>
-                    <el-option label="No Coinbase" value="false"></el-option>
-                </el-select>
-                </el-col> -->
-                <el-col :span="4">
-                    <el-button type="primary" @click="fetchAddress">Search</el-button>
-                </el-col>
-            </el-row>
-
             <div v-for="transaction in displayedTransactions" :key="transaction.txid" :class="getTransactionClass(transaction.role)" class="transaction-item">
                 <el-tag effect="dark" class="coinbase" v-if="transaction.is_coinbase">Coinbase</el-tag>
                 <div>Transaction ID: {{ transaction.txid }}</div>
